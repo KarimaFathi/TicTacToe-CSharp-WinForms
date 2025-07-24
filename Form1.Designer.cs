5e347bc4d1e370bd15f0994ed9b3c79c096af087
KarimaFathi <karimafathifadili@gmail.com>
2025-07-24 17:22:15 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace Tic_Tac_toe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pic3I1 = new System.Windows.Forms.PictureBox();
+            this.pic1I2 = new System.Windows.Forms.PictureBox();
+            this.pic1I3 = new System.Windows.Forms.PictureBox();
+            this.pic2I3 = new System.Windows.Forms.PictureBox();
+            this.pic2I2 = new System.Windows.Forms.PictureBox();
+            this.pic3I3 = new System.Windows.Forms.PictureBox();
+            this.pic3I2 = new System.Windows.Forms.PictureBox();
+            this.pic2I1 = new System.Windows.Forms.PictureBox();
+            this.pic1I1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3I1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1I2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1I3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2I3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2I2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3I3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3I2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2I1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1I1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Swis721 Blk BT", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(521, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(418, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tic-Tac-Toe Game";
+            // 
+            // pic3I1
+            // 
+            this.pic3I1.Image = global::Tic_Tac_toe.Properties.Resources.question_mark_96;
+            this.pic3I1.Location = new System.Drawing.Point(438, 439);
+            this.pic3I1.Name = "pic3I1";
+            this.pic3I1.Size = new System.Drawing.Size(152, 107);
+            this.pic3I1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic3I1.TabIndex = 9;
+            this.pic3I1.TabStop = false;
+            this.pic3I1.Click += new System.EventHandler(this.pic3I1_Click);
+            // 
+            // pic1I2
+            // 
+            this.pic1I2.Image = global::Tic_Tac_toe.Properties.Resources.question_mark_96;
+            this.pic1I2.Location = new System.Drawing.Point(632, 130);
+            this.pic1I2.Name = "pic1I2";
+            this.pic1I2.Size = new System.Drawing.Size(152, 107);
+            this.pic1I2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1I2.TabIndex = 8;
+            this.pic1I2.TabStop = false;
+            this.pic1I2.Click += new System.EventHandler(this.pic1I2_Click);
+            // 
+            // pic1I3
+            // 
+            this.pic1I3.Image = global::Tic_Tac_toe.Properties.Resources.question_mark_96;
+            this.pic1I3.Location = new System.Drawing.Point(842, 130);
+            this.pic1I3.Name = "pic1I3";
+            this.pic1I3.Size = new System.Drawing.Size(152, 107);
+            this.pic1I3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1I3.TabIndex = 7;
+            this.pic1I3.TabStop = false;
+            this.pic1I3.Click += new System.EventHandler(this.pic1I3_Click);
+            // 
+            // pic2I3
+            // 
+            this.pic2I3.Image = global::Tic_Tac_toe.Properties.Resources.question_mark_96;
+            this.pic2I3.Location = new System.Drawing.Point(842, 287);
+            this.pic2I3.Name = "pic2I3";
+            this.pic2I3.Size = new System.Drawing.Size(152, 107);
+            this.pic2I3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic2I3.TabIndex = 6;
+            this.pic2I3.TabStop = false;
+            this.pic2I3.Click += new System.EventHandler(this.pic2I3_Click);
+            // 
+            // pic2I2
+            // 
+            this.pic2I2.Image = global::Tic_Tac_toe.Properties.Resources.question_mark_96;
+            this.pic2I2.Location = new System.Drawing.Point(632, 287);
+            this.pic2I2.Name = "pic2I2";
+            this.pic2I2.Size = new System.Drawing.Size(152, 107);
+            this.pic2I2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic2I2.TabIndex = 5;
+            this.pic2I2.TabStop = false;
+            this.pic2I2.Click += new System.EventHandler(this.pic2I2_Click);
+            // 
+            // pic3I3
+            // 
+            this.pic3I3.Image = global::Tic_Tac_toe.Properties.Resources.question_mark_96;
+            this.pic3I3.Location = new System.Drawing.Point(842, 439);
+            this.pic3I3.Name = "pic3I3";
+            this.pic3I3.Size = new System.Drawing.Size(152, 107);
+            this.pic3I3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic3I3.TabIndex = 4;
+            this.pic3I3.TabStop = false;
+            this.pic3I3.Click += new System.EventHandler(this.pic3I3_Click);
+            // 
+            // pic3I2
+            // 
+            this.pic3I2.Image = global::Tic_Tac_toe.Properties.Resources.question_mark_96;
+            this.pic3I2.Location = new System.Drawing.Point(632, 439);
+            this.pic3I2.Name = "pic3I2";
+            this.pic3I2.Size = new System.Drawing.Size(152, 107);
+            this.pic3I2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic3I2.TabIndex = 3;
+            this.pic3I2.TabStop = false;
+            this.pic3I2.Click += new System.EventHandler(this.pic3I2_Click);
+            // 
+            // pic2I1
+            // 
+            this.pic2I1.Image = global::Tic_Tac_toe.Properties.Resources.question_mark_96;
+            this.pic2I1.Location = new System.Drawing.Point(438, 287);
+            this.pic2I1.Name = "pic2I1";
+            this.pic2I1.Size = new System.Drawing.Size(152, 107);
+            this.pic2I1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic2I1.TabIndex = 2;
+            this.pic2I1.TabStop = false;
+            this.pic2I1.Click += new System.EventHandler(this.pic2I1_Click);
+            // 
+            // pic1I1
+            // 
+            this.pic1I1.Image = global::Tic_Tac_toe.Properties.Resources.question_mark_96;
+            this.pic1I1.Location = new System.Drawing.Point(438, 130);
+            this.pic1I1.Name = "pic1I1";
+            this.pic1I1.Size = new System.Drawing.Size(152, 107);
+            this.pic1I1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pic1I1.TabIndex = 1;
+            this.pic1I1.TabStop = false;
+            this.pic1I1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pic1I1_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1082, 603);
+            this.Controls.Add(this.pic3I1);
+            this.Controls.Add(this.pic1I2);
+            this.Controls.Add(this.pic1I3);
+            this.Controls.Add(this.pic2I3);
+            this.Controls.Add(this.pic2I2);
+            this.Controls.Add(this.pic3I3);
+            this.Controls.Add(this.pic3I2);
+            this.Controls.Add(this.pic2I1);
+            this.Controls.Add(this.pic1I1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Tic-Tac-Toe Game";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.pic3I1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1I2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1I3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2I3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2I2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3I3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic3I2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic2I1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic1I1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pic1I1;
+        private System.Windows.Forms.PictureBox pic2I1;
+        private System.Windows.Forms.PictureBox pic3I2;
+        private System.Windows.Forms.PictureBox pic3I3;
+        private System.Windows.Forms.PictureBox pic2I2;
+        private System.Windows.Forms.PictureBox pic2I3;
+        private System.Windows.Forms.PictureBox pic1I3;
+        private System.Windows.Forms.PictureBox pic1I2;
+        private System.Windows.Forms.PictureBox pic3I1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_toe
+{
+    enum enPlayer { player1 = 1, player2 = 2 };
+    public partial class Form1 : Form
+    {
+        enPlayer player = enPlayer.player1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Color white = Color.White;
+            Pen Pen = new Pen(white);
+
+            Pen.Width = 10;
+
+            Pen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+            Pen.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+
+            e.Graphics.DrawLine(Pen, 400, 85, 400, 370);
+            e.Graphics.DrawLine(Pen, 540, 85, 540, 370);
+            e.Graphics.DrawLine(Pen, 290, 175, 650, 175);
+            e.Graphics.DrawLine(Pen, 290, 270, 650, 270);
+        }
+        private void pic1I1_MouseClick(object sender, MouseEventArgs e)
+        {
+           
+            if (pic1I1.Tag == null)
+            {
+                if (player == enPlayer.player1)
+                {
+                    pic1I1.Image = Properties.Resources.X;
+                    player = enPlayer.player2;
+                }
+                else
+                {
+                    pic1I1.Image = Properties.Resources.O;
+                    player = enPlayer.player1;
+                }
+                pic1I1.Tag = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pic1I2_Click(object sender, EventArgs e)
+        {
+            if (pic1I2.Tag == null)
+            {
+                if (player == enPlayer.player1)
+                {
+                    pic1I2.Image = Properties.Resources.X;
+                    player = enPlayer.player2;
+                }
+                else
+                {
+                    pic1I2.Image = Properties.Resources.O;
+                    player = enPlayer.player1;
+                }
+                pic1I2.Tag = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pic1I3_Click(object sender, EventArgs e)
+        {
+            if (pic1I3.Tag == null)
+            {
+                if (player == enPlayer.player1)
+                {
+                    pic1I3.Image = Properties.Resources.X;
+                    player = enPlayer.player2;
+                }
+                else
+                {
+                    pic1I3.Image = Properties.Resources.O;
+                    player = enPlayer.player1;
+                }
+                pic1I3.Tag = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pic2I1_Click(object sender, EventArgs e)
+        {
+
+            if (pic2I1.Tag == null)
+            {
+                if (player == enPlayer.player1)
+                {
+                    pic2I1.Image = Properties.Resources.X;
+                    player = enPlayer.player2;
+                }
+                else
+                {
+                    pic2I1.Image = Properties.Resources.O;
+                    player = enPlayer.player1;
+                }
+                pic2I1.Tag = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pic2I2_Click(object sender, EventArgs e)
+        {
+            if (pic2I2.Tag == null)
+            {
+                if (player == enPlayer.player1)
+                {
+                    pic2I2.Image = Properties.Resources.X;
+                    player = enPlayer.player2;
+                }
+                else
+                {
+                    pic2I2.Image = Properties.Resources.O;
+                    player = enPlayer.player1;
+                }
+                pic2I2.Tag = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pic2I3_Click(object sender, EventArgs e)
+        {
+            if (pic2I3.Tag == null)
+            {
+                if (player == enPlayer.player1)
+                {
+                    pic2I3.Image = Properties.Resources.X;
+                    player = enPlayer.player2;
+                }
+                else
+                {
+                    pic2I3.Image = Properties.Resources.O;
+                    player = enPlayer.player1;
+                }
+                pic2I3.Tag = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pic3I1_Click(object sender, EventArgs e)
+        {
+            if (pic3I1.Tag == null)
+            {
+                if (player == enPlayer.player1)
+                {
+                    pic3I1.Image = Properties.Resources.X;
+                    player = enPlayer.player2;
+                }
+                else
+                {
+                    pic3I1.Image = Properties.Resources.O;
+                    player = enPlayer.player1;
+                }
+                pic3I1.Tag = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pic3I2_Click(object sender, EventArgs e)
+        {
+            if (pic3I2.Tag == null)
+            {
+                if (player == enPlayer.player1)
+                {
+                    pic3I2.Image = Properties.Resources.X;
+                    player = enPlayer.player2;
+                }
+                else
+                {
+                    pic3I2.Image = Properties.Resources.O;
+                    player = enPlayer.player1;
+                }
+                pic3I2.Tag = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pic3I3_Click(object sender, EventArgs e)
+        {
+            if (pic3I3.Tag == null)
+            {
+                if (player == enPlayer.player1)
+                {
+                    pic3I3.Image = Properties.Resources.X;
+                    player = enPlayer.player2;
+                }
+                else
+                {
+                    pic3I3.Image = Properties.Resources.O;
+                    player = enPlayer.player1;
+                }
+                pic3I3.Tag = true;
+            }
+            else
+            {
+                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
