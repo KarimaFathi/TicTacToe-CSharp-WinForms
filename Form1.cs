14963f814cf0e02d71a104ad1a1fa85a18b1e352
KarimaFathi <karimafathifadili@gmail.com>
2025-07-26 11:15:53 +0100

    Refactor : Unify player move logic across all cells

--- Form1.cs
+++ Form1.cs
@@ -34,251 +34,83 @@ namespace Tic_Tac_toe
             e.Graphics.DrawLine(Pen, 290, 175, 650, 175);
             e.Graphics.DrawLine(Pen, 290, 270, 650, 270);
         }
-        private void pic1I1_MouseClick(object sender, MouseEventArgs e)
+
+        private void HandlePlayerMove(PictureBox picBox)
         {
-           
-            if (pic1I1.Tag == null)
+            if (picBox.Tag != null)
             {
-                if (player == enPlayer.player1)
-                {
-                    pic1I1.Image = Properties.Resources.X;
-                    player = enPlayer.player2;
-                    lbPlayerTurn.Text = "Player 2";
-                    pic1I1.Tag = "X";
-                }
-                else
-                {
-                    pic1I1.Image = Properties.Resources.O;
-                    player = enPlayer.player1;
-                    lbPlayerTurn.Text = "Player 1";
-                    pic1I1.Tag = "O";
-                }
-               
+                MessageBox.Show("Invalid move. Please choose an empty cell.", "Invalid Move", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (player == enPlayer.player1)
+            {
+                picBox.Image = Properties.Resources.X;
+                picBox.Tag = "X";
+                player = enPlayer.player2;
+                lbPlayerTurn.Text = "Player 2";
             }
             else
             {
-                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                picBox.Image = Properties.Resources.O;
+                picBox.Tag = "O";
+                player = enPlayer.player1;
+                lbPlayerTurn.Text = "Player 1";
             }
-            checkWinner();
+
+            CheckWinner();
+        }
+        private void pic1I1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+            HandlePlayerMove(pic1I1);
         }
 
         private void pic1I2_Click(object sender, EventArgs e)
         {
-            if (pic1I2.Tag == null)
-            {
-                if (player == enPlayer.player1)
-                {
-                    pic1I2.Image = Properties.Resources.X;
-                    player = enPlayer.player2;
-                    lbPlayerTurn.Text = "Player 2";
-                    pic1I2.Tag = "X";
-                }
-                else
-                {
-                    pic1I2.Image = Properties.Resources.O;
-                    player = enPlayer.player1;
-                    lbPlayerTurn.Text = "Player 1";
-                    pic1I2.Tag = "O";
-                }
-              
-            }
-            else
-            {
-                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            checkWinner();
+            HandlePlayerMove(pic1I2);
         }
 
         private void pic1I3_Click(object sender, EventArgs e)
         {
-            if (pic1I3.Tag == null)
-            {
-                if (player == enPlayer.player1)
-                {
-                    pic1I3.Image = Properties.Resources.X;
-                    player = enPlayer.player2;
-                    lbPlayerTurn.Text = "Player 2";
-                    pic1I3.Tag = "X";
-                }
-                else
-                {
-                    pic1I3.Image = Properties.Resources.O;
-                    player = enPlayer.player1;
-                    lbPlayerTurn.Text = "Player 1";
-                    pic1I3.Tag = "O";
-                }
-            }
-            else
-            {
-                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            checkWinner();
+            HandlePlayerMove(pic1I3);
         }
 
         private void pic2I1_Click(object sender, EventArgs e)
         {
 
-            if (pic2I1.Tag == null)
-            {
-                if (player == enPlayer.player1)
-                {
-                    pic2I1.Image = Properties.Resources.X;
-                    player = enPlayer.player2;
-                    lbPlayerTurn.Text = "Player 2";
-                    pic2I1.Tag = "X";
-                }
-                else
-                {
-                    pic2I1.Image = Properties.Resources.O;
-                    player = enPlayer.player1;
-                    lbPlayerTurn.Text = "Player 1";
-                    pic2I1.Tag = "O";
-                }
-               
-            }
-            else
-            {
-                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            checkWinner();
+            HandlePlayerMove(pic2I1);
         }
 
         private void pic2I2_Click(object sender, EventArgs e)
         {
-            if (pic2I2.Tag == null)
-            {
-                if (player == enPlayer.player1)
-                {
-                    pic2I2.Image = Properties.Resources.X;
-                    player = enPlayer.player2;
-                    lbPlayerTurn.Text = "Player 2";
-                    pic2I2.Tag = "X";
-                }
-                else
-                {
-                    pic2I2.Image = Properties.Resources.O;
-                    player = enPlayer.player1;
-                    lbPlayerTurn.Text = "Player 1";
-                    pic2I2.Tag = "O";
-                }
-                
-            }
-            else
-            {
-                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            checkWinner();
+
+            HandlePlayerMove(pic2I2);
         }
 
         private void pic2I3_Click(object sender, EventArgs e)
         {
-            if (pic2I3.Tag == null)
-            {
-                if (player == enPlayer.player1)
-                {
-                    pic2I3.Image = Properties.Resources.X;
-                    player = enPlayer.player2;
-                    lbPlayerTurn.Text = "Player 2";
-                    pic2I3.Tag = "X";
-                }
-                else
-                {
-                    pic2I3.Image = Properties.Resources.O;
-                    player = enPlayer.player1;
-                    lbPlayerTurn.Text = "Player 1";
-                    pic2I3.Tag = "O";
-                }
-               
-            }
-            else
-            {
-                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            checkWinner();
+
+            HandlePlayerMove(pic2I3);
         }
 
         private void pic3I1_Click(object sender, EventArgs e)
         {
-            if (pic3I1.Tag == null)
-            {
-                if (player == enPlayer.player1)
-                {
-                    pic3I1.Image = Properties.Resources.X;
-                    player = enPlayer.player2;
-                    lbPlayerTurn.Text = "Player 2";
-                    pic3I1.Tag = "X";
-                }
-                else
-                {
-                    pic3I1.Image = Properties.Resources.O;
-                    player = enPlayer.player1;
-                    lbPlayerTurn.Text = "Player 1";
-                    pic3I1.Tag = "O";
-                }
-               
-            }
-            else
-            {
-                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            checkWinner();
+
+            HandlePlayerMove(pic3I1);
         }
 
         private void pic3I2_Click(object sender, EventArgs e)
         {
-            if (pic3I2.Tag == null)
-            {
-                if (player == enPlayer.player1)
-                {
-                    pic3I2.Image = Properties.Resources.X;
-                    player = enPlayer.player2;
-                    lbPlayerTurn.Text = "Player 2";
-                    pic3I2.Tag = "X";
-                }
-                else
-                {
-                    pic3I2.Image = Properties.Resources.O;
-                    player = enPlayer.player1;
-                    lbPlayerTurn.Text = "Player 1";
-                    pic3I2.Tag = "O";
-                }
-              
-            }
-            else
-            {
-                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            checkWinner();
+            HandlePlayerMove(pic3I2);
         }
 
         private void pic3I3_Click(object sender, EventArgs e)
         {
-            if (pic3I3.Tag == null)
-            {
-                if (player == enPlayer.player1)
-                {
-                    pic3I3.Image = Properties.Resources.X;
-                    player = enPlayer.player2;
-                    lbPlayerTurn.Text = "Player 2";
-                    pic3I3.Tag = "X";
-                }
-                else
-                {
-                    pic3I3.Image = Properties.Resources.O;
-                    player = enPlayer.player1;
-                    lbPlayerTurn.Text = "Player 1";
-                    pic3I3.Tag = "O";
-                }
-         
-            }
-            else
-            {
-                MessageBox.Show("Wrong choice", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            checkWinner();
+            HandlePlayerMove(pic3I3);
         }
 
-        private void checkWinner()
+        private void CheckWinner()
         {
            
             if (
